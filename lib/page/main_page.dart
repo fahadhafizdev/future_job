e34947b8ac2ac87ad9b6f888e40238ae33f6173f
fahadhafiz dev <fahadhafiz.dev@gmail.com>
2021-09-29 17:44:59 +0700

    create custom icon navigation

--- lib/page/main_page.dart
+++ lib/page/main_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:future_job/page/account_page.dart';
+import 'package:future_job/page/favorit_page.dart';
 import 'package:future_job/page/home_page.dart';
+import 'package:future_job/page/notification_page.dart';
 import 'package:future_job/providers/page_provider.dart';
 import 'package:future_job/theme.dart';
 import 'package:provider/provider.dart';
@@ -13,49 +16,107 @@ class MainPage extends StatelessWidget {
       switch (currentIndex) {
         case 0:
           return HomePage();
+        case 1:
+          return NotificationPage();
+        case 2:
+          return FavoritPage();
+        case 3:
+          return AccountPage();
         default:
           return HomePage();
       }
     }
 
+    // Widget customButtonNavigation() {
+    //   return Align(
+    //     alignment: Alignment.bottomCenter,
+    //     child: Container(
+    //       margin: EdgeInsets.only(top: 30),
+    //       child: BottomNavigationBar(
+    //         type: BottomNavigationBarType.fixed,
+    //         showSelectedLabels: false,
+    //         unselectedItemColor: Color(0xffB3B5C4),
+    //         selectedItemColor: Color(0xff272C2F),
+    //         elevation: 0,
+    //         currentIndex: 0,
+    //         iconSize: 24,
+    //         items: [
+    //           BottomNavigationBarItem(
+    //             icon: ImageIcon(
+    //               AssetImage('assets/images/icon_apps.png'),
+    //             ),
+    //             label: '',
+    //           ),
+    //           BottomNavigationBarItem(
+    //             icon: ImageIcon(
+    //               AssetImage('assets/images/icon_notification.png'),
+    //             ),
+    //             label: '',
+    //           ),
+    //           BottomNavigationBarItem(
+    //             icon: ImageIcon(
+    //               AssetImage('assets/images/icon_love.png'),
+    //             ),
+    //             label: '',
+    //           ),
+    //           BottomNavigationBarItem(
+    //             icon: ImageIcon(
+    //               AssetImage('assets/images/icon_user.png'),
+    //             ),
+    //             label: '',
+    //           )
+    //         ],
+    //       ),
+    //     ),
+    //   );
+    // }
+
     Widget customButtonNavigation() {
       return Align(
         alignment: Alignment.bottomCenter,
         child: Container(
-          margin: EdgeInsets.only(top: 30),
-          child: BottomNavigationBar(
-            type: BottomNavigationBarType.fixed,
-            showSelectedLabels: false,
-            unselectedItemColor: Color(0xffB3B5C4),
-            selectedItemColor: Color(0xff272C2F),
-            elevation: 0,
-            currentIndex: 0,
-            iconSize: 24,
-            items: [
-              BottomNavigationBarItem(
-                icon: ImageIcon(
-                  AssetImage('assets/images/icon_apps.png'),
+          height: 84,
+          width: 360,
+          color: whiteColor,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container(
+                height: 24,
+                width: 24,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/icon_apps.png'),
+                  ),
                 ),
-                label: '',
               ),
-              BottomNavigationBarItem(
-                icon: ImageIcon(
-                  AssetImage('assets/images/icon_notification.png'),
+              Container(
+                height: 24,
+                width: 24,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/icon_apps.png'),
+                  ),
                 ),
-                label: '',
               ),
-              BottomNavigationBarItem(
-                icon: ImageIcon(
-                  AssetImage('assets/images/icon_love.png'),
+              Container(
+                height: 24,
+                width: 24,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/icon_apps.png'),
+                  ),
                 ),
-                label: '',
               ),
-              BottomNavigationBarItem(
-                icon: ImageIcon(
-                  AssetImage('assets/images/icon_user.png'),
+              Container(
+                height: 24,
+                width: 24,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/icon_apps.png'),
+                  ),
                 ),
-                label: '',
-              )
+              ),
             ],
           ),
         ),
---
+++ lib/widgets/custom_navigation_icon.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:future_job/theme.dart';
+
+class CustomIconNavigation extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
